320aea705cdcb9b77de74c83b810b2fa49115a82
bookhabit <qnrgoqlt@naver.com>
2023-01-16 02:12:31 +0900

    글로벌 스타일 지정

---
+++ styles/GlobalStyle.tsx
@@ -0,0 +1,19 @@
+import reset from "styled-reset";
+import palette from "./palette";
+import { createGlobalStyle,css } from "styled-components";
+
+const globalStyle = css`
+    ${reset};
+    *{
+        box-sizing: border-box;
+    }
+    body{
+        color:${palette.black}
+    }
+`
+
+const GlobalStyle = createGlobalStyle`
+    ${globalStyle}
+`
+
+export default GlobalStyle;
