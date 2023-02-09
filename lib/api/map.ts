88525f6364b1e35bdc6fa5cc10da81616cb004cd
bookhabit <qnrgoqlt@naver.com>
2023-02-09 21:48:16 +0900

    숙소 위치 검색하기

--- lib/api/map.ts
+++ lib/api/map.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import description from './../../pages/room/register/description';
 
 type GetLocationInfoAPIResponse = {
     latitude:number,
@@ -13,3 +14,12 @@ type GetLocationInfoAPIResponse = {
 // 현재 위치 정보 가져오기 api
 export const getLocationInfoAPI = async ({latitude,longitude}:{latitude:number; longitude:number;})=> axios.get<GetLocationInfoAPIResponse>(`/api/maps/location?latitude=${latitude}&longitude=${longitude}`)
 
+// 구글 장소 검색 api
+export const searchPlacesAPI  = (keyword:string)=>{
+    return axios.get<{description:string;placeId:string}[]>(`/api/maps/places?keyword=${keyword}`)
+}
+
+// placeId로 장소 정보 가져오기
+export const getPlaceAPI  = (placeId:string)=>{
+    return axios.get<{location:string;latitude:number;longitude:number}>(`/api/maps/places/${placeId}`)
+}
